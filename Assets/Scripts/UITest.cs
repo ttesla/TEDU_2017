3801621a3ce89391f53717aebff30189e95579a1
ttesla <omer.akyol@gmail.com>
2017-04-15 15:18:47 +0300

    ders update

---
+++ Assets/Scripts/UITest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UITest : MonoBehaviour
+{
+    
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    public void OnMyButtonClick()
+    {
+        Debug.Log("BUTTON PRESSED!");
+       // SceneManager.LoadScene()
+    }
+}
